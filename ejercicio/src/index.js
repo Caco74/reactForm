04db331b32b56e1ff3260ba98aaf144233c7e498
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2019-09-30 23:34:50 -0300

    commit002

--- ejercicio/src/index.js
+++ ejercicio/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var form = React.createElement('form', { action: '' },
+    <h2>Formulario de Registro</h2>,
+    <div class="input-container">
+        <i class="fa fa-user icon"></i>
+        <input class="input-field" type="text" placeholder="Nombre de usuario" name="usrnm" />
+    </div>,
+    <div class="input-container">
+        <i class="fa fa-envelope icon"></i>
+        <input class="input-field" type="text" placeholder="Correo Electrónico" name="email" />
+    </div>,
+    <div class="input-container">
+        <i class="fa fa-key icon"></i>
+        <input class="input-field" type="password" placeholder="Contraseña" name="psw" />
+    </div>,
+    <button type="submit" class="btn"><strong>Regístrate</strong></button>);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<div class='form'>{form}</div>, document.getElementById('root'));
+
+
 serviceWorker.unregister();
